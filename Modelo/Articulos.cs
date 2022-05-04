2d19de826a15f0209cce5c46be56fdd10b0797af
Nahuel <Nahuel@DESKTOP-VSS5SI3>
2022-05-03 21:07:30 -0300

    Agregar archivos de proyecto.

---
+++ Modelo/Articulos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Modelo
+{
+    class Articulos
+    {
+        public int ID { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marcas IdMarca { get; set; }
+        public Categorias IdCategoria { get; set; }
+        public string ImageUrl { get; set; }
+        public float Precio { get; set; }
+
+    }
+}
